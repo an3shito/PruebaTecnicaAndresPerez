8b1fb06e8c9fa6a13e764f8cbe87863febfbfdf8
Andres Perez <and.dres2294@gmail.com>
2024-06-05 01:43:11 -0600

    Se envian los scripts de un proyecto MVC creado en Visual Studio para Login.

---
+++ Login/CuentaController.cs
@@ -0,0 +1,28 @@
+﻿using Login.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Login.Controllers
+{
+    public class CuentaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // POST: Cuenta/Login, se crea un metodo post para enviar la informcaión si la autenticación se realiza con exito.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Aquí puedes agregar la lógica de autenticación
+                TempData["Message"] = "Proceso realizado con Éxito!";
+                return RedirectToAction("Login");
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ Login/LoginViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Login.Models
+{
+    public class LoginViewModel
+    {
+
+        [Required(ErrorMessage = "Usuario Requerido.")]
+        public required string Username { get; set; }
+
+        [Required(ErrorMessage = "Contraseña Requerida")]
+        public required string Password { get; set; }
+    }
+}
